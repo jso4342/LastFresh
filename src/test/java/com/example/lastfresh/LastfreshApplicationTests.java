0746730f7b3a5757f74e5505ae704c3430ec9f8f
s701090 <sim701090@gmail.com>
2022-04-28 15:55:35 +0900

    초기환경세팅

---
+++ src/test/java/com/example/lastfresh/LastfreshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lastfresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastfreshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
