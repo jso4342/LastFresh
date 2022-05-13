ef7dbad5c9dd58c24e8b954acc523eed9e744c3c
JJinJong <akwnsldj1@naver.com>
2022-05-13 13:26:11 +0900

    기초 설정

--- src/main/java/com/example/lastfresh/controller/sell/SellController.java
+++ src/main/java/com/example/lastfresh/controller/sell/SellController.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @Slf4j
@@ -20,12 +19,6 @@ public class SellController {
     @GetMapping("/sellMenuList")
     public void sellMenuList(){}
 
-    @GetMapping("/sellMenuRegister")
-    public void sellMenuRegister(){}
-
-    @GetMapping("/test")
-    public RedirectView test(){
-        return new RedirectView("/main/main.html");
-    }
+    @GetMapping("/sellMenuRezzegister(){}
 
 }
