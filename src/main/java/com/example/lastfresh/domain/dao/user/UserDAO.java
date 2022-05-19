e3b00971f7fb30cc1393c7b257ea8e2cd31e906f
JinyongHyun <wlsdyd4270@gmail.com>
2022-05-19 22:17:23 +0900

    현진용 로그인 커밋(복호화 미구현)

--- src/main/java/com/example/lastfresh/domain/dao/user/UserDAO.java
+++ src/main/java/com/example/lastfresh/domain/dao/user/UserDAO.java
@@ -1,14 +1,23 @@
 package com.example.lastfresh.domain.dao.user;
 
 
+import com.example.lastfresh.domain.vo.UserVO;
 import com.example.lastfresh.mapper.user.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Map;
 
 @Repository
 @RequiredArgsConstructor
 public class UserDAO {
     private final UserMapper userMapper;
+
+    public Long login(String userId, String userPw) {return userMapper.login(userId, userPw);}
+
+
+
+
 //    public  getList( ) {}
 //    public void register( ) {}
 //    public boolean modify( ) {}
