6db560a849d9edd107a17dbf7baf90a802ae76fd
s701090 <sim701090@gmail.com>
2022-05-19 23:27:03 +0900

    창환 커밋

--- src/main/java/com/example/lastfresh/service/user/BasketService.java
+++ src/main/java/com/example/lastfresh/service/user/BasketService.java
@@ -1,8 +1,17 @@
 package com.example.lastfresh.service.user;
 
+import com.example.lastfresh.domain.dao.user.BasketDAO;
+import com.example.lastfresh.domain.vo.BasketVO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface BasketService {
+@RequiredArgsConstructor
+@Slf4j
+public class BasketService {
+    private final BasketDAO basketDAO;
+    //  장바구니에 상품 담기
+    public void insert(BasketVO basketVO){basketDAO.insert(basketVO);}
 
 }
--- src/main/resources/static/front/assets/js/main/main.js
+++ src/main/resources/static/front/assets/js/main/main.js
@@ -31,18 +31,20 @@ $.each($category, function(index, item){
 
 
 
-var cart = $(".cart_option");
-var button = $(".cancel");
+let cart = $(".cart_option");
+let button = $(".cancel");
 $(".innerLatChanceCart").click(function(){
     $(cart).removeClass("off");
     $(cart).css("opacity", "1");
     $(".bg_loading").css("display", "block");
+    $("#cartPut").css("display", "block");
 });
 
 $(".innerCart").click(function(){
     $(cart).removeClass("off");
     $(cart).css("opacity", "1");
     $(".bg_loading").css("display", "block");
+    $("#cartPut").css("display", "block");
 });
 
 $(button).click(function(){
@@ -50,9 +52,49 @@ $(button).click(function(){
     $(cart).css("opacity", "0");
     $(".bg_loading").css("display", "none");
 });
-
-$(document).ready(function() {
-    $(cart).addClass("off");
-    $(cart).css("opacity", "0");
-    $(".bg_loading").css("display", "none");
+$(document).ready( function() {
+    $(".result").val(1);
 });
+let total=0;
+
+// function count(type)  {
+//     // 결과를 표시할 element
+//     let resultElement = $(".result");
+//     let afterPrice =$(".num");
+//     // 현재 화면에 표시된 값
+//     // let number = resultElement.val();
+//     // let beforePrice =($(".dcPriceNum").innerHTML);
+//     console.log(total)
+//     // 더하기/빼기
+//     $(".toBasket").each(function(i, item) {
+//         //일반금액
+//         let price=($(".newPrice")[i].innerHTML);
+//         $(".dc_price").html(price);
+//         //합계 전 가격
+//         let beforePrice =($(".dcPriceNum")[i].innerHTML);
+//         $(".num").html(beforePrice);
+//         console.log(beforePrice)
+//         //수량
+//         let number = resultElement.val();
+//
+//         if (type === 'plus') {
+//             number = parseInt(number) + 1;
+//             console.log(number)
+//             total = beforePrice * number;
+//             console.log(total)
+//         } else if (type === 'minus') {
+//             number = parseInt(number) - 1;
+//             total = (beforePrice / number);
+//             console.log(total)
+//             if (number == 0) {
+//                 return
+//             }
+//         }
+//         resultElement.val(number);
+//         afterPrice.html(total);
+//     });
+//
+//     // 내용 포멧
+//
+//     // 결과 출력
+// }
