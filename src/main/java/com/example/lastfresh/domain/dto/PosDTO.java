923829f1fefdce6f7b16a874186c480b99bed811
JJinJong <akwnsldj1@naver.com>
2022-05-23 10:58:18 +0900

    config update

---
+++ src/main/java/com/example/lastfresh/domain/dto/PosDTO.java
@@ -0,0 +1,23 @@
+package com.example.lastfresh.domain.dto;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class PosDTO {
+    private Long billProductListNum;
+    private Long sellProductNum;
+    private Long billOrderNum;
+    private Long userNum;
+    private Long billProductQuantity;
+    private Long billProductPrice;
+    private String billDeliveryMethod;
+    private String billStatus;
+    private Long billCookingTime;
+    private String billOrderDate;
+    private String billOrderDateTime;
+    private String sellProductName;
+    private String userPhone;
+    private Long sellUserNum;
+}
