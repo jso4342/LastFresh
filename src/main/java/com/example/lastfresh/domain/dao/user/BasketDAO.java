c23e9d62fc1904c0081464d5e74723444372c9dc
jso4342 <jso4342@naver.com>
2022-05-26 16:02:24 +0900

    BasketController, BasketDAO

--- src/main/java/com/example/lastfresh/domain/dao/user/BasketDAO.java
+++ src/main/java/com/example/lastfresh/domain/dao/user/BasketDAO.java
@@ -18,9 +18,29 @@ public class BasketDAO {
 
     public List<BasketVO> getListByUserNum(Long userNum) {return basketMapper.getListByUserNum(userNum);}
     public List<BasketDTO> getListByDTO(Long userNum){return basketMapper.getListByDTO(userNum);}
+    public List<BasketDTO> getListPickUp(Long userNum){return basketMapper.getListPickUp(userNum);}
+    public List<BasketDTO> getListDelivery(Long userNum){return basketMapper.getListDelivery(userNum);}
+    public List<BasketDTO> getListShipping(Long userNum){return basketMapper.getListShipping(userNum);}
+
 
     public void removeItems(Long userNum){basketMapper.removeItems(userNum);}
-    
+
+    public void remove(Long basketNum){basketMapper.remove(basketNum);}
+
+    public void removeAll(Long userNum){basketMapper.removeAll(userNum);}
+
+    public void addQuantity(Long basketNum){basketMapper.addQuantity(basketNum);}
+
+    public void subtractQuantity(Long basketNum){basketMapper.subtractQuantity(basketNum);}
+
+    public void check(Long basketNum){basketMapper.check(basketNum);}
+
+    public void checkAll(Long userNum){basketMapper.checkAll(userNum);}
+
+    public void unCheckAll(Long userNum){basketMapper.unCheckAll(userNum);}
+
+    public int count(Long userNum){ return basketMapper.count(userNum);}
+
     //  장바구니에 상품 담기
     public void insert(BasketVO basketVO){basketMapper.insert(basketVO);}
 
